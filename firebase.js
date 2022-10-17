7024510174613c13ad2e0d4b73e9ab1ab4243aff
nsect <nsecttheory@gmail.com>
2022-10-17 16:03:14 -0700

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyB4_rhB9wbI5Fgxxj--vvijtyxl2B5Qqqk',
+  authDomain: 'nsect-netflix.firebaseapp.com',
+  projectId: 'nsect-netflix',
+  storageBucket: 'nsect-netflix.appspot.com',
+  messagingSenderId: '769148585415',
+  appId: '1:769148585415:web:f0a7155b8cbecdc4c8c5e2',
+};
---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', transitionNavBar);
+    return () => window.removeEventListener('scroll', transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav__black'}`}>
+      <div className="nav__content">
+        <img
+          className="nav__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt="logo"
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+export default Nav;
